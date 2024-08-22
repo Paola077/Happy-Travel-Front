4adb27d9c26908d408c7fc23d7d3b98b08a5807c
BCN-Asumiko <yakoshko@gmail.com>
2024-08-22 16:42:37 +0200

    config/ add React+Vite, add TailwindCSS, add Material Tailwind dependencies

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import ButtonExample from './components/ButtonExample'
+
+function App() {
+  
+  return (
+    <>
+    <div className="p-4 bg-blue-500 text-white">
+      Tailwind CSS is working!
+    </div>
+    <ButtonExample/>
+    </>
+  )
+}
+
+export default App
