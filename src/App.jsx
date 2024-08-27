708016f2ca6c0d7606d017b749a46f927924fae3
MabelZar <mabel.zar.esp@gmail.com>
2024-08-27 14:39:04 +0200

    card HeaderUser complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,20 @@ import './App.css';
 // import ButtonExample from './components/ButtonExample'
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
+import DestinationCardUser from './components/cardDestination/DestinationCardUser';
+import HeaderUser from './components/header/HeaderUser';
+import images from "../public/assets/images.jpg";
 
 function App() {
 
   return (
     <React.StrictMode> 
     <RouterProvider router={router}/>
+    <HeaderUser/>
+    <DestinationCardUser
+            imageSrc={images}
+            title="Islas Azores"
+            subtitle="Portugal"/>
     {/* <div className="p-4 bg-blue-500 text-white">
       Tailwind CSS is working!
     </div>
