6e7b9a131a993734ea5d9b3369463f62d5513a58
MabelZar <mabel.zar.esp@gmail.com>
2024-08-28 17:39:50 +0200

    alert component created

--- src/components/modal/ConfirmModal.jsx
+++ src/components/modal/ConfirmModal.jsx
@@ -11,11 +11,11 @@ const ConfirmModal = ({ isOpen, onClose, onConfirm }) => {
         className="bg-black bg-opacity-50 absolute inset-0"
         onClick={onClose}
       ></div>
-      <div className="bg-yellow-100 rounded-[1.25rem] w-[22.75rem] p-8 z-10 shadow-lg ">
-        <p className="text-blue-600 mb-6  text-center">¿Quieres eliminar este destino?</p>
+      <div className="bg-yellow-100 rounded-[1.25rem] w-[31.25rem] h-[12.5rem] p-8 z-10 shadow-lg ">
+        <p className="text-blue-600 mb-8 text-2xl text-center mt-6">¿Quieres eliminar este destino?</p>
         <div className="flex justify-center space-x-4">
           <>
-            <AcceptButton onClick={onConfirm} />
+            <AcceptButton onClick={onConfirm}/>
             <CancelButton onClick={onClose} />
           </>
         </div>
