8a81a7ba29b98bb28a16e86a29226d248dfc3033
Cris <crispr.zde@gmail.com>
2024-08-28 18:21:57 +0200

    fetch data logic created

--- src/components/card/DestinationCardUser.jsx
+++ src/components/card/DestinationCardUser.jsx
@@ -4,13 +4,15 @@ import InfoButton from "../buttons/InfoButton";
 import { useContext } from "react";
 import { AuthContext } from "../../auth/AuthWrapper";
 
-function DestinationCardUser({ imageSrc, title, subtitle }) {
+function DestinationCardUser({ destination, currentUser }) {
+  
   const {user, authToken} = useContext(AuthContext);
   const isAuthenticated = authToken !== null;
+  const isCreator = destination.creatorId === currentUser.id;
 
   return (
     <div className="bg-yellow-100 w-[18.75rem] h-[23.313rem] rounded-[1.25rem] shadow-lg overflow-hidden relative">
-      <img src={imageSrc} alt={title} className="w-full h-[18.75rem] rounded-[1.25rem] object-cover" />
+      <img src={destination.url_image} alt={title} className="w-full h-[18.75rem] rounded-[1.25rem] object-cover" />
       {isAuthenticated && (
         <div className="absolute top-2 right-2">
           <InfoButton />
@@ -18,11 +20,11 @@ function DestinationCardUser({ imageSrc, title, subtitle }) {
       <div className=" p-4 flex  justify-between">
         <div className="flex flex-col justify-between flex-grow">
           <div>
-            <h2 className="text-lg font-bold text-blue-600">{title}</h2>
-            <p className="text-sm text-blue-600">{subtitle}</p>
+            <h2 className="text-lg font-bold text-blue-600">{destination.title}</h2>
+            <p className="text-sm text-blue-600">{destination.location}</p>
           </div>
         </div>
-        {isAuthenticated && (
+        {isCreator && (
           <div className="w-[5.063rem]  h-[2.50rem] flex justify-between">
             <EditButton />
             <DeleteButton />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import HeaderUser from "../components/header/HeaderUser";
 import DestinationCardUser from "../components/card/DestinationCardUser";
-import images from '../../public/assets/images.jpg'
+import { GET_DESTINATIONS_URL } from "../config/urls";
+import { apiRequest } from "../services/apiRequest";
+import { AuthContext } from "../auth/AuthWrapper";
 
 
 const Home = () => {
+    const [destinations, setDestinations] = useState([]);
+    const {authToken} = useContext(AuthContext);
+
+    console.log("Auth Token:", authToken);
+
+
+    useEffect(()=>{
+        const fetchDestinations  = async () => {
+            try{
+                const headers = authToken ? { Authorization: `Bearer ${authToken}` } : {};
+                console.log("Headers:", headers);
+                const data = await apiRequest(GET_DESTINATIONS_URL, 'GET', null, headers);
+                setDestinations(data)
+            }catch (error) {
+                console.error('Error al obtener los destinos: ', error);
+            }
+        };
+        fetchDestinations ();
+    }, [authToken]);
     return(
         <>
             <HeaderUser />
-            <DestinationCardUser 
-            imageSrc={images}
-            title="Islas Azores"
-            subtitle="Portugal"/>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {destinations.map((destination) => (
+                    <DestinationCardUser 
+                    key={destination.id}
+                    imageSrc={destination.url_image}
+                    title={destination.title}
+                    subtitle={destination.location}/>
+                ))}
+            </div>
+            
         </>
     )
 }
