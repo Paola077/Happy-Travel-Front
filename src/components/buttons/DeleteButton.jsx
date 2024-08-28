b94002f40110cdefa58b753a114a3874889cc869
MabelZar <mabel.zar.esp@gmail.com>
2024-08-28 15:33:38 +0200

    modal alert created

--- src/components/buttons/DeleteButton.jsx
+++ src/components/buttons/DeleteButton.jsx
@@ -1,4 +1,4 @@
-import Delete from "../../../public/assets/Delete-icon.svg";
+/* import Delete from "../../../public/assets/Delete-icon.svg";
 import { useNavigate } from "react-router-dom";
 function DeleteButton() {
   const navigate = useNavigate();
@@ -16,4 +16,44 @@ function DeleteButton() {
     </>
   );
 }
+export default DeleteButton;
+ */
+import React, { useState } from 'react';
+import Delete from "../../../public/assets/Delete-icon.svg";
+import ConfirmModal from '../modal/ConfirmModal'; // Asegúrate de que la ruta sea correcta
+
+const DeleteButton = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleDelete = () => {
+    setModalOpen(true);
+  };
+
+  const handleConfirm = () => {
+    setModalOpen(false);
+    console.log('Destino eliminado');
+    //Añadir lógica para eliminar el destino
+  };
+
+  const handleCancel = () => {
+    setModalOpen(false);
+  };
+
+  return (
+    <><img
+    src={Delete}
+    className="h-[2.5rem] w-[1.917rem] cursor-pointer"
+    onClick={handleDelete}
+  />
+    <div>
+      <ConfirmModal 
+        isOpen={isModalOpen} 
+        onClose={handleCancel} 
+        onConfirm={handleConfirm}
+      />
+    </div>
+    </>
+  );
+};
+
 export default DeleteButton;
