f6c04a93fcab0073d9923253488e4d7fbf9cb6c2
BCN-Asumiko <yakoshko@gmail.com>
2024-08-29 19:42:37 +0200

    fix issue with Edit Form

--- src/components/card/DestinationCardUser.jsx
+++ src/components/card/DestinationCardUser.jsx
@@ -2,37 +2,33 @@ import DeleteButton from "../buttons/DeleteButton";
 import EditButton from "../buttons/EditButton";
 import InfoButton from "../buttons/InfoButton";
 
-
 function DestinationCardUser({ destination, currentUser }) {
     const userId = localStorage.getItem('userId'); 
     const isAuthenticated = !!currentUser;
     const isCreator = currentUser && destination.user && destination.user.id.toString() === userId;
 
-    console.log('currentUser:', currentUser);
-    console.log('destination user:', destination.user);
-
     return (
         <div className="bg-yellow-100 w-[18.75rem] h-[23.313rem] rounded-[1.25rem] shadow-lg overflow-hidden relative">
-            <img src={destination.urlImage} alt={destination.title} className="w-full h-[18.75rem] rounded-[1.25rem] object-cover" />
-            {isAuthenticated && (
-                <div className="absolute top-2 right-2">
-                    <InfoButton />
-                </div>
-            )}
-            <div className="p-4 flex justify-between">
-                <div className="flex flex-col justify-between flex-grow">
-                    <div>
-                        <h2 className="text-lg font-bold text-blue-600">{destination.title}</h2>
-                        <p className="text-sm text-blue-600">{destination.location}</p>
-                    </div>
-                </div>
-                {isCreator && ( 
-                    <div className="w-[5.063rem] h-[2.50rem] flex justify-between">
-                        <EditButton />
-                        <DeleteButton />
-                    </div>
-                )}
+        <img src={destination.urlImage} alt={destination.title} className="w-full h-[18.75rem] rounded-[1.25rem] object-cover" />
+        {isAuthenticated && (
+            <div className="absolute top-2 right-2">
+            <InfoButton />
             </div>
+        )}
+        <div className="p-4 flex justify-between">
+            <div className="flex flex-col justify-between flex-grow">
+            <div>
+                <h2 className="text-lg font-bold text-blue-600">{destination.title}</h2>
+                <p className="text-sm text-blue-600">{destination.location}</p>
+            </div>
+            </div>
+            {isCreator && ( 
+            <div className="w-[5.063rem] h-[2.50rem] flex justify-between">
+                <EditButton destinationId={destination.id} />
+                <DeleteButton />
+            </div>
+            )}
+        </div>
         </div>
     );
 }
